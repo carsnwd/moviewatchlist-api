0b844acee61d9fc94c5722fa83dba1822d72a361
carsnwd <carsnwd@gmail.com>
2025-02-01 11:43:03 -0500

    setup a basic auth page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { FirebaseAuthGuard } from '@/auth/firebase-auth/firebase-auth.guard';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  
-  @Get('/secret')
-  @UseGuards(FirebaseAuthGuard)
-  getSecretRoute(): string {
-    return 'Secret!!!!';
-  }
+  constructor() { }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { AppService } from './app.service';
 import { FirebaseService } from './firebase/firebase.service';
 import { AuthController } from './auth/auth.controller';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src'),
+      exclude: ['/api*'],
+    }),
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, FirebaseService],
 })
-export class AppModule {}
+export class AppModule { }
